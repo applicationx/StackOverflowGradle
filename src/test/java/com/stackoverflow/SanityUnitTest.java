f2cdafa7336d5c0435ac36200492a3d8dd326a31
Jonas Dämfors <jonas@handlar.nu>
2017-06-05 00:22:29 +0200

    Passing and failing test

---
+++ src/test/java/com/stackoverflow/SanityUnitTest.java
@@ -0,0 +1,15 @@
+package com.stackoverflow;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by jonasd on 2017-06-05.
+ */
+public class SanityUnitTest {
+
+    @Test
+    public void simpleTest() {
+        Assert.assertEquals(true, true);
+    }
+}
